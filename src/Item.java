ff804ea6f0caa09c2a937ca3612644943060db30
khanhsf145 <157661275+khanhsf145@users.noreply.github.com>
2024-03-14 07:42:26 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,23 @@
 public class Item {
+  String name;
+    private String description;
+    protected String ID;
+    float price;
+
+    Item(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void showInfo() {}
 }

000429a24ab3b21c4cad9000ad66d68739f7a888
khanhsf145 <157661275+khanhsf145@users.noreply.github.com>
2024-03-14 07:42:54 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
 public class Book {
+  private String author, genre;
+    private int numberOfPage;
+    Book(String name, String description, String ID, float price, String author, String genre, int numberOfPage) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.genre = genre;
+        this.numberOfPage = numberOfPage;
+    }
+
+    public void showInfo() {
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Author: " + author);
+        System.out.println("Genre: " + genre);
+        System.out.println("Discription: " + getDescription());
+        System.out.println("Number of page: " + numberOfPage);
+        System.out.println("Price: " + price);
+    }
 }

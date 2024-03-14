57bfad7db1a55722fa6cfd63efc74da95a93ff0b
khanhsf145 <157661275+khanhsf145@users.noreply.github.com>
2024-03-14 07:44:37 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("Tình yêu màu nắng", "Hay", "B1", 45000, "3/7/2015");
+        Book book = new Book("Sách giáo khoa Toán 12", "...", "T12", 14000, "Nhà xuất bản Giáo dục Việt Nam", "Giáo dục", 65);
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
         book.showInfo();
-
     }
 }
